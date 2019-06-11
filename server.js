2520bd10162e7bf0feb54cc27bd4faf87c1ca924
Noe <noeliasebastian@gmail.com>
2019-06-11 23:08:31 +0200

    Primer commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+var hbs = require('hbs');
+
+require('./helpers/helpers');
+
+const port = process.env.PORT || 3000
+
+app.use(express.static(__dirname + '/public'));
+
+// Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'noe'
+    });
+})
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+// app.get('/data', (req, res) => {
+//     res.send('Hola data');
+// })
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+});
